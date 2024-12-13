099b1b0b1d79cfdd20fab2ab659f575e945000b3
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-13 10:20:17 +0500

    Day-07

---
+++ 13-12-2024/app.js
@@ -0,0 +1,26 @@
+// Problem #09:-
+// Create a function that check if a word is palindrome or not.
+
+// function isPalindrome(str) {
+//   const word = str.split("").reverse().join("");
+//   console.log(word, "===>", str);
+//   return word === str;
+// }
+
+// console.log(isPalindrome("Level")); // true
+// console.log(isPalindrome("Right")); // False
+
+// const str = "level";
+// const isPalindrome = () => {
+//   let revresedStr = "";
+//   for (let j = str.length - 1; j >= 0; j--) {
+//     revresedStr += str[j];
+//   }
+//   console.log(revresedStr, "===>", str);
+//   return revresedStr === str;
+// };
+// console.log(isPalindrome());
+
+// const str = "level";
+// const splittedStr = [...str].reverse().join("");
+// console.log(str === splittedStr);
